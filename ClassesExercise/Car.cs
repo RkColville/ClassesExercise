dd090256dad050b99bcdadd438826c257786521a
Robert Colville <rkcolville@gmail.com>
2021-04-20 15:46:18 -0500

    initial commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "FJ";
+            myCar.Year = 2014;
+
+            var tesla = new Car()
+            {
+                Make = "Tesla",
+                Model = "S",
+                Year = 2021
+            };
+
+            var Lexus = new Car("Lexus", "GX", 2001);
+            var carList = new List<Car>() { myCar, tesla, Lexus, };
+            //Console.WriteLine(carList.Capacity);
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+            
+            
+
+
+
+        }
+    }
+}
